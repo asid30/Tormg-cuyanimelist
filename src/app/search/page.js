545c8277b644b7f57792bde30ae42b803736f2ab
Abdilah Asyidiqi <williamcopper2003@gmail.com>
2023-12-31 22:47:43 +0700

    belajar

---
+++ src/app/search/page.js
@@ -0,0 +1,10 @@
+const Page = () => {
+    return(
+        <>
+            <h1>Ini Search Page</h1>
+            <p>Page mesin pencari gan!</p>
+        </>
+    )
+}
+
+export default Page
---
+++ src/components/Search/index.js
@@ -0,0 +1,7 @@
+const Search = () => {
+    return(
+        <div>Search Komponen</div>
+    )
+}
+
+export default Search;
